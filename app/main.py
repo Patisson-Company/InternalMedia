0be78a9f5354b0f6713ba0a8db0ca587d1c3cdbf
EliseyGodX <krotov.ruslan.31@gmail.com>
2024-12-20 23:36:40 +0300

    v0.1.0

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import config
 from api import router
 from fastapi import FastAPI
 from patisson_appLauncher.fastapi_app_launcher import UvicornFastapiAppLauncher
-from patisson_request.service_routes import UsersRoute
+from patisson_request.service_routes import InternalMediaRoute
 
 
 @asynccontextmanager
@@ -18,15 +18,17 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title=config.SERVICE_NAME, lifespan=lifespan)
 
 if __name__ == "__main__":
+    health_path = f'/{config.SERVICE_NAME}/{InternalMediaRoute.health().path}'
+    
     app_launcher = UvicornFastapiAppLauncher(app, router,
                         service_name=config.SERVICE_NAME,
                         host=config.SERVICE_HOST)
     app_launcher.add_token_middleware(
         config.SelfService.get_access_token,
-        excluded_paths=[f'/{UsersRoute.health().path}']
+        excluded_paths=[health_path]
         )
     app_launcher.add_sync_consul_health_path()
-    app_launcher.consul_register(check_path=f'/{config.SERVICE_NAME}/health')
+    app_launcher.consul_register(health_path)
     app_launcher.add_jaeger()
     app_launcher.include_router(prefix=f'/{config.SERVICE_NAME}')
     app_launcher.app_run()
